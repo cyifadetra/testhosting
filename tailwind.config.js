923b80f2dae521a00da893bb3772e6ccd880a775
Cyifa Detra Raharja <101962974+cyifadetra@users.noreply.github.com>
2024-07-16 16:54:32 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
